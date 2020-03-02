1906020f84252d9326c28140341c5e454ec63a09
jkomendant <jennifer.komendant@gmail.com>
2020-03-02 15:53:53 -0500

    cashier money unchanged with exception thrown

---
+++ src/komendant/Scrabble/ScrabbleFrame.java
@@ -0,0 +1,4 @@
+package komendant.Scrabble;
+
+public class ScrabbleFrame {
+}
--- src/komendant/cashier/Cashier.java
+++ src/komendant/cashier/Cashier.java
@@ -23,42 +23,6 @@ public class Cashier {
         if (cashierCash.calculateTotalCashAmount() == 0) {
             throw new NotEnoughChangeException();
         }
-        while (totalCash != 0) {
-            if (totalCash >= 20.00 && customerCash.getAmount20Dollar() != 0) {
-                cashierCash.increase20Dollar(customerCash.getAmount20Dollar());
-                totalCash -= (20.00 * customerCash.getAmount20Dollar());
-                customerCash.decrease20Dollar(customerCash.getAmount20Dollar());
-            } else if (totalCash >= 10.00 && customerCash.getAmount10Dollar() != 0) {
-                cashierCash.increase10Dollar(customerCash.getAmount10Dollar());
-                totalCash -= (10.00 * customerCash.getAmount10Dollar());
-                customerCash.decrease10Dollar(customerCash.getAmount10Dollar());
-            } else if (totalCash >= 5.00 && customerCash.getAmount5Dollar() != 0) {
-                cashierCash.increase5Dollar(customerCash.getAmount5Dollar());
-                totalCash -= (5.00 * customerCash.getAmount5Dollar());
-                customerCash.decrease5Dollar(customerCash.getAmount5Dollar());
-            } else if (totalCash >= 1.00 && customerCash.getAmount1Dollar() != 0) {
-                cashierCash.increase1Dollar(customerCash.getAmount1Dollar());
-                totalCash -= (1.00 * customerCash.getAmount1Dollar());
-                customerCash.decrease1Dollar(customerCash.getAmount1Dollar());
-            } else if (totalCash >= 0.25 && customerCash.getAmountQuarter() != 0) {
-                cashierCash.increaseQuarter(customerCash.getAmountQuarter());
-                totalCash -= (0.25 * customerCash.getAmountQuarter());
-                customerCash.decreaseQuarter(customerCash.getAmountQuarter());
-            } else if (totalCash >= 0.10 && customerCash.getAmountDime() != 0) {
-                cashierCash.increaseDime(customerCash.getAmountDime());
-                totalCash -= (0.10 * customerCash.getAmountDime());
-                customerCash.decreaseDime(customerCash.getAmountDime());
-            } else if (totalCash >= 0.05 && customerCash.getAmountNickle() != 0) {
-                cashierCash.increaseNickle(customerCash.getAmountNickle());
-                totalCash -= (0.05 * customerCash.getAmountNickle());
-                customerCash.decreaseNickle(customerCash.getAmountNickle());
-            } else if (totalCash >= 0.01 && customerCash.getAmountPenny() != 0) {
-                cashierCash.increasePenny(customerCash.getAmountPenny());
-                totalCash -= (0.01 * customerCash.getAmountPenny());
-                customerCash.decreasePenny(customerCash.getAmountPenny());
-            }
-        }
-
         Cash change = new Cash(0, 0, 0, 0, 0,
                 0, 0, 0);
 
@@ -96,13 +60,64 @@ public class Cashier {
                 change.increasePenny(1);
                 totalChange = Double.parseDouble(df.format((totalChange - 0.01)));
             } else {
+                cashierCash.increasePenny(change.getAmountPenny());
+                change.decreasePenny(change.getAmountPenny());
+                cashierCash.increaseNickle(change.getAmountNickle());
+                change.decreaseNickle(change.getAmountNickle());
+                cashierCash.increaseDime(change.getAmountDime());
+                change.decreaseDime(change.getAmountDime());
+                cashierCash.increaseQuarter(change.getAmountQuarter());
+                change.decreaseQuarter(change.getAmountQuarter());
+                cashierCash.increase1Dollar(change.getAmount1Dollar());
+                change.decrease1Dollar(change.getAmount1Dollar());
+                cashierCash.increase5Dollar(change.getAmount5Dollar());
+                change.decrease5Dollar(change.getAmount5Dollar());
+                cashierCash.increase1Dollar(change.getAmount1Dollar());
+                change.decrease10Dollar(change.getAmount10Dollar());
+                cashierCash.increase20Dollar(change.getAmount20Dollar());
+                change.decrease20Dollar(change.getAmount20Dollar());
                 throw new NotEnoughChangeException();
+            }
+        }
+        while (totalCash != 0) {
+            if (totalCash >= 20.00 && customerCash.getAmount20Dollar() != 0) {
+                cashierCash.increase20Dollar(customerCash.getAmount20Dollar());
+                totalCash -= (20.00 * customerCash.getAmount20Dollar());
+                customerCash.decrease20Dollar(customerCash.getAmount20Dollar());
+            } else if (totalCash >= 10.00 && customerCash.getAmount10Dollar() != 0) {
+                cashierCash.increase10Dollar(customerCash.getAmount10Dollar());
+                totalCash -= (10.00 * customerCash.getAmount10Dollar());
+                customerCash.decrease10Dollar(customerCash.getAmount10Dollar());
+            } else if (totalCash >= 5.00 && customerCash.getAmount5Dollar() != 0) {
+                cashierCash.increase5Dollar(customerCash.getAmount5Dollar());
+                totalCash -= (5.00 * customerCash.getAmount5Dollar());
+                customerCash.decrease5Dollar(customerCash.getAmount5Dollar());
+            } else if (totalCash >= 1.00 && customerCash.getAmount1Dollar() != 0) {
+                cashierCash.increase1Dollar(customerCash.getAmount1Dollar());
+                totalCash -= (1.00 * customerCash.getAmount1Dollar());
+                customerCash.decrease1Dollar(customerCash.getAmount1Dollar());
+            } else if (totalCash >= 0.25 && customerCash.getAmountQuarter() != 0) {
+                cashierCash.increaseQuarter(customerCash.getAmountQuarter());
+                totalCash -= (0.25 * customerCash.getAmountQuarter());
+                customerCash.decreaseQuarter(customerCash.getAmountQuarter());
+            } else if (totalCash >= 0.10 && customerCash.getAmountDime() != 0) {
+                cashierCash.increaseDime(customerCash.getAmountDime());
+                totalCash -= (0.10 * customerCash.getAmountDime());
+                customerCash.decreaseDime(customerCash.getAmountDime());
+            } else if (totalCash >= 0.05 && customerCash.getAmountNickle() != 0) {
+                cashierCash.increaseNickle(customerCash.getAmountNickle());
+                totalCash -= (0.05 * customerCash.getAmountNickle());
+                customerCash.decreaseNickle(customerCash.getAmountNickle());
+            } else if (totalCash >= 0.01 && customerCash.getAmountPenny() != 0) {
+                cashierCash.increasePenny(customerCash.getAmountPenny());
+                totalCash -= (0.01 * customerCash.getAmountPenny());
+                customerCash.decreasePenny(customerCash.getAmountPenny());
             }
         }
         return change;
     }
 
-    protected class NotEnoughChangeException extends Exception {
+    public class NotEnoughChangeException extends Exception {
 
     }
 }
